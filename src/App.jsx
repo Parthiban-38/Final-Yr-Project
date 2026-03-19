9ef8a01e16a7c62375085260e708c7fc29583616
Rubesh <krubes833@gmail.com>
2026-03-19 10:08:50 +0530

    Added Recommender component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
-import { useEffect } from "react";
-import AppRoutes from "./routes";
+import Recommender from "./pages/Recommender";
 
 function App() {
-  useEffect(() => {
-    const removeGoogleBanner = () => {
-      const iframe = document.querySelector(".goog-te-banner-frame");
-
-      if (iframe) {
-        iframe.style.display = "none";
-      }
-      document.body.style.top = "0px";
-    };
-
-    const interval = setInterval(removeGoogleBanner, 100);
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
+  return <Recommender />;
 }
 
 export default App;
